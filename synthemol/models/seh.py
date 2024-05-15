30654cb59e9e5401da0d57691e18d1b5a2e3b9b3
Andrei <a.rekesh@mail.utoronto.ca>
2024-05-15 01:15:26 +0000

    All tasks, caching issue fixed

--- synthemol/models/seh.py
+++ synthemol/models/seh.py
@@ -26,25 +26,21 @@ class SEHProxy():
         self.beta = params.beta
         self.model.to(self.device)
 
-    def reward_transform(self, rewards):
-        return rewards
-
     def __call__(self, smiles: List[str]) -> np.array:
-        mols = [Chem.MolFromSmiles(s) for s in smiles]
-        graphs = [mol2graph(i) for i in mols]
-        smiles_to_save = [Chem.MolToSmiles(mol) for mol in mols]
-        is_valid = torch.tensor([i is not None for i in graphs]).bool()
-        batch = gd.Batch.from_data_list([i for i in graphs if i is not None])
+        mol = Chem.MolFromSmiles(smiles)
+        graph = mol2graph(mol)
+        smiles_to_save = [smiles]
+        batch = gd.Batch.from_data_list([graph])
         batch.to(self.device)
         self.model.to(self.device)
-        raw_scores = self.model(batch).reshape((-1,)).data.cpu()
-        raw_scores[raw_scores.isnan()] = 0
-        scores_to_save = list(raw_scores)
-        #with open(os.path.join(self.log_dir, "visited.txt"), 'a') as file:
-        #    # Write each molecule and its score to the file
-        #    for molecule, score in zip(smiles_to_save, scores_to_save):
-        #        file.write(f"{molecule}, {score}\n")
+        raw_score = self.model(batch).reshape((-1,)).data.cpu()
+        raw_score[raw_score.isnan()] = 0
+        score_to_save = list(raw_score)
+        with open(os.path.join(self.log_dir, "visited.txt"), 'a') as file:
+            # Write each molecule and its score to the file
+            for molecule, score in zip(smiles_to_save, score_to_save):
+                file.write(f"{molecule}, {score}\n")
 
-        transformed_scores = self.reward_transform(raw_scores).clip(1e-4, 100).reshape((-1,))
+        transformed_score = raw_score.clip(1e-4, 100).reshape((-1,))
         #print(f"Proxy Mean: {raw_scores.mean()}, Proxy Max: {raw_scores.max()}, Mean Reward: {transformed_scores.mean()}, Max Reward: {transformed_scores.max()}")
-        return np.array(transformed_scores, dtype=float)
+        return transformed_score[0]
